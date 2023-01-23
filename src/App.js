726f2a971dff2b9a39cca56f9125d5b79f3eb8b9
Anna Miroshnichenko <miroshnichenkoanna74@gmail.com>
2023-01-23 20:09:20 +0300

    Add play text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const alanBtnInst = useRef(alanBtn({
+    key: '7b3d8453594be70256b1cbd1c96e3cc02e956eca572e1d8b807a3e2338fdd0dc/stage'
+  })).current;
+
+  useEffect(() => {
+    alanBtnInst.activate().then(() => {
+      alanBtnInst.playText('Hello, here is custom text');
+    })
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
